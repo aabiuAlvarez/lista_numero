8c6c30727a09c63c19e173286af917c5c5656840
aabiuAlvarez <aabiujosmar@gmail.com>
2025-03-14 09:49:26 -0600

    lista_numeros

---
+++ lista_numeros.py
@@ -0,0 +1,8 @@
+# Lista de números
+numeros = [1, 2, 3, 4, 5, 6, 7, 8]
+a, *c, b = numeros  
+
+print("Lista original:", numeros)
+print("Primer número (a):", a)
+print("Último número (b):", b)
+print("Números intermedios (c):", c)
